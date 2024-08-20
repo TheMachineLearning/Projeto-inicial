efeefca7cedcd68c11ad0110bd0221a8c0bf3ce8
RogerioJoseBarros <deeplearningsparkhadoop@gmail.com>
2024-08-20 16:54:08 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Jornada de dados")
+
+#Variáveis
+idade = 34
+nome = "Rogerio"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade +1
+print(nova_idade)
+
+# Lista
+
+lista = [1,2,3,4,5]
+
+lista_invertida = lista.reverse()
+
+for i in range(5):
+    print(i)
+  
